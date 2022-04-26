65e73ca74d82bacf146f49e666b3c576e536f978
Guillaume Keerhem <guillaumekeerhem@mbpdeguillaume.lille.fr.ibm.com>
2022-04-26 16:52:32 +0200

    Added logic to make the panel and setup buttons default position

--- ep-front/components/buttonRotation/index.tsx
+++ ep-front/components/buttonRotation/index.tsx
@@ -1,29 +1,58 @@
-import React, { ComponentType, useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 // import SElectrikBoard from "./style";
+interface ButtonRotationProps {
+  setCircuitPos: React.Dispatch<
+    React.SetStateAction<{ type: string; pos: number }>
+  >;
+  circuit: { type: string; pos: number };
+}
 
-const ButtonRotation: ComponentType = () => {
-  const [rotateNeeded, setRotateNeeded]: [boolean, Function] = useState(false);
-  const [oneRotate, setOneRotate]: [number, Function] = useState(0);
+const ButtonRotation: React.ComponentType<ButtonRotationProps> = ({
+  setCircuitPos,
+  circuit,
+}) => {
+  const [rotateNeeded, setRotateNeeded] = useState<boolean>(false);
+  const [oneRotate, setOneRotate] = useState<number>(0);
+
+  const style: { transform: string; transition: string } = {
+    transform: `rotate(${oneRotate}deg)`,
+    transition: 'transform 70ms ease',
+  };
 
   const retrieveAngle = () => {
     setOneRotate(oneRotate === 270 ? 0 : oneRotate + 90);
   };
 
-  const rotateClick = () => {
-    setRotateNeeded(!rotateNeeded);
-    retrieveAngle();
+  const posHandler = (currCircuit: typeof circuit) => {
+    setCircuitPos({
+      ...currCircuit,
+      [currCircuit.pos]:
+        currCircuit.pos === 270
+          ? (currCircuit.pos = 0)
+          : (currCircuit.pos += 90),
+    });
   };
+
+  const rotateClick = (targetCircuit: typeof circuit) => {
+    if (targetCircuit.pos !== 1) {
+      setRotateNeeded(!rotateNeeded);
+      retrieveAngle();
+      posHandler(targetCircuit);
+    }
+  };
+
+  useEffect(() => {
+    circuit.pos !== 1 && setOneRotate(circuit.pos);
+  }, []);
 
   return (
     <button
-      style={{
-        transform: `rotate(${oneRotate}deg)`,
-        transition: "transform 70ms ease",
-      }}
-      onClick={rotateClick}
+      style={style}
+      onClick={() => rotateClick(circuit)}
       //   className={`square${index}`}
     >
-      {/* {index} */} une image
+      {/* {index} */}{' '}
+      {circuit.pos === 1 ? 'G' : `${circuit.type}${circuit.pos}`}
     </button>
   );
 };
