5e7dc956cf006c3cd9989eecd1826f5c1420764c
Guillaume Keerhem <guillaumekeerhem@MacBook-Pro-de-Guillaume.local>
2022-04-28 16:16:22 +0200

    Added the WaterGage components to test

--- ep-front/pages/games/[id]/step3.tsx
+++ ep-front/pages/games/[id]/step3.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from "next";
-import Link from "next/link";
-import React from "react";
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import React from 'react';
+import WaterGage from '../../../components/waterGage';
 
 const Step3: NextPage = () => {
   return (
@@ -8,6 +9,7 @@ const Step3: NextPage = () => {
       <h1>This is step three !</h1>
       <Link href="/games/id/step4">Next step</Link>
       <Link href="/games/id/step2">Previous step</Link>
+      <WaterGage />
     </>
   );
 };
